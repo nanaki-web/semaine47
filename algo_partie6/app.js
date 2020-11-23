fe3ecbd355968819e18d5466fcd09ade70758b0c
Anne Wilga <anne.wilga@orange.fr>
2020-11-23 17:19:43 +0100

    partie 6 et 7 algo

--- algo_partie6/app.js
+++ algo_partie6/app.js
@@ -1,7 +1,81 @@
-alert("texte");
-let Truc = new Array ();
-for (i=0;i<6;i++)
-{
-    Truc[i] = 0;
-}
-console.log(Truc)
+// alert("texte");
+// let Truc = new Array ();
+// for (i=0;i<6;i++)
+// {
+//     Truc[i] = 0;
+// }
+// console.log(Truc)
+
+//exercice 6.2
+
+// let truc = new Array();
+//     truc [0] = "a";
+//     truc [1] = "e";
+//     truc [2] = "i";
+//     truc [3] = "o";
+//     truc [4] = "u";
+//     truc [5] = "y";
+
+// console.log(truc);
+
+// exercice 6.3
+
+// let tableau = new Array();
+//     for(i=0;i<7;i++)
+//     {
+//         tableau = window.prompt("entrez la note numero ");
+//         console.log(tableau);
+//     }
+
+    //exercice 6.4
+
+// let nb= new Array();
+// for(i=0;i<5;i++)
+// {
+//     nb[i] = i * i ;
+// }
+// for(i=0;i<5;i++)
+// {
+//     console.log(nb);
+// }
+
+
+// let nb= new Array();
+// for(i=0;i<5;i++)
+// {
+//     nb[i] = i * i ;
+   
+// }
+// console.log(nb);
+
+// exercice 6.5 
+
+// let N = new Array();
+
+// N[0] =1
+
+// for(k=1;k<6;k++)
+// {
+//     N= [k-1]+2;
+// }
+// for(i=0;i<6;i++)
+// {
+//     console.log(N);
+// }
+
+// exercice 6.3
+// let notes = new Array();
+// som = 0;
+//     for(i=0;i<7;i++)
+//     {
+//         notes= window.prompt("entrez la note numero ",i+1);
+//         console.log(notes);
+//         som = som + notes ;
+//     }
+  
+//     console.log("la moyenne est de :",som/9);
+let t = new Array();
+nb = window.prompt("entrez le nombre de valeurs");
+console.log(nb);
+redim t[nb-1];
+
